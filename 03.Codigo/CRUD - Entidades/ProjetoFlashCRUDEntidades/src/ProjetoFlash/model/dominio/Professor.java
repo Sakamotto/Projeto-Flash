8e2d21d213b87a95ba3345b8961ea9f4c108d9cc
Danilo de Oliveira <danilo.psn94@gmail.com>
2016-09-24 21:44:36 -0300

    CRUD professor em memória RAM

---
+++ 03.Codigo/CRUD - Entidades/ProjetoFlashCRUDEntidades/src/ProjetoFlash/model/dominio/Professor.java
@@ -0,0 +1,41 @@
+package ProjetoFlash.model.dominio;
+
+
+
+public class Professor {
+
+    private String nome, matricula, cpf;
+    private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
