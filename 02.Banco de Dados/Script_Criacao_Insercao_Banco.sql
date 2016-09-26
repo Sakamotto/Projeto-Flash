909327b0dc63be73735f1065a92389ea77c20901
Danilo de Oliveira <danilo.psn94@gmail.com>
2016-09-26 15:31:28 -0300

    Banco de dados atualizado.

--- 02.Banco de Dados/Script_Criacao_Insercao_Banco.sql
+++ 02.Banco de Dados/Script_Criacao_Insercao_Banco.sql
@@ -1,4 +1,12 @@
-﻿
+﻿CREATE TABLE flash.espaco (
+                espaco_id INTEGER NOT NULL,
+                tipo VARCHAR NOT NULL,
+                bloco VARCHAR NOT NULL,
+                numero INTEGER NOT NULL,
+                CONSTRAINT espaco_pk PRIMARY KEY (espaco_id)
+);
+
+
 CREATE SEQUENCE flash.dia_semana_dia_semana_id_seq_1;
 
 CREATE TABLE flash.dia_semana (
@@ -35,11 +43,25 @@ CREATE TABLE flash.curso (
 
 ALTER SEQUENCE flash.curso_curso_id_seq_1 OWNED BY flash.curso.curso_id;
 
+CREATE SEQUENCE flash.horario_horario_id_seq;
+
+CREATE TABLE flash.horario (
+                horario_id INTEGER NOT NULL DEFAULT nextval('flash.horario_horario_id_seq'),
+                hora_inicio TIME NOT NULL,
+                hora_fim TIME NOT NULL,
+                dia_semana_id INTEGER NOT NULL,
+                CONSTRAINT horario_pk PRIMARY KEY (horario_id)
+);
+
+
+ALTER SEQUENCE flash.horario_horario_id_seq OWNED BY flash.horario.horario_id;
+
 CREATE SEQUENCE flash.endereco_endereco_id_seq_1;
 
 CREATE TABLE flash.endereco (
                 endereco_id INTEGER NOT NULL DEFAULT nextval('flash.endereco_endereco_id_seq_1'),
-                rua VARCHAR NOT NULL,
+                numero INTEGER NOT NULL,
+                endereco VARCHAR NOT NULL,
                 bairro VARCHAR NOT NULL,
                 municipio VARCHAR NOT NULL,
                 cep VARCHAR NOT NULL,
@@ -80,19 +102,22 @@ CREATE TABLE flash.disciplina (
 
 ALTER SEQUENCE flash.disciplina_disciplina_id_seq OWNED BY flash.disciplina.disciplina_id;
 
-CREATE SEQUENCE flash.horario_horario_id_seq;
-
-CREATE TABLE flash.horario (
-                horario_id INTEGER NOT NULL DEFAULT nextval('flash.horario_horario_id_seq'),
-                hora_inicio TIME NOT NULL,
-                hora_fim TIME NOT NULL,
+CREATE TABLE flash.alocacao (
+                alocacao_id INTEGER NOT NULL,
+                horario_id INTEGER NOT NULL,
                 disciplina_id INTEGER NOT NULL,
-                dia_semana_id INTEGER NOT NULL,
-                CONSTRAINT horario_pk PRIMARY KEY (horario_id)
+                espaco_id INTEGER NOT NULL,
+                professor_id INTEGER NOT NULL,
+                CONSTRAINT alocacao_pk PRIMARY KEY (alocacao_id)
 );
 
 
-ALTER SEQUENCE flash.horario_horario_id_seq OWNED BY flash.horario.horario_id;
+ALTER TABLE flash.alocacao ADD CONSTRAINT espaco_alocacao_fk
+FOREIGN KEY (espaco_id)
+REFERENCES flash.espaco (espaco_id)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+NOT DEFERRABLE;
 
 ALTER TABLE flash.horario ADD CONSTRAINT dia_semana_horario_fk
 FOREIGN KEY (dia_semana_id)
@@ -105,7 +130,7 @@ ALTER TABLE flash.disciplina ADD CONSTRAINT area_conhecimento_disciplina_fk
 FOREIGN KEY (area_conhecimento_id)
 REFERENCES flash.area_conhecimento (area_conhecimento_id)
 ON DELETE NO ACTION
-ON UPDATE NO ACTION
+ON UPDATE CASCADE
 NOT DEFERRABLE;
 
 ALTER TABLE flash.disciplina ADD CONSTRAINT curso_disciplina_fk
@@ -115,32 +140,46 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+ALTER TABLE flash.alocacao ADD CONSTRAINT horario_alocacao_fk
+FOREIGN KEY (horario_id)
+REFERENCES flash.horario (horario_id)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+NOT DEFERRABLE;
+
 ALTER TABLE flash.professor ADD CONSTRAINT endereco_professor_fk
 FOREIGN KEY (endereco_id)
 REFERENCES flash.endereco (endereco_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
+ON DELETE CASCADE
+ON UPDATE CASCADE
 NOT DEFERRABLE;
 
 ALTER TABLE flash.disciplina ADD CONSTRAINT professor_disciplina_fk
 FOREIGN KEY (professor_id)
 REFERENCES flash.professor (professor_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
+ON DELETE SET NULL
+ON UPDATE CASCADE
+NOT DEFERRABLE;
+
+ALTER TABLE flash.alocacao ADD CONSTRAINT professor_alocacao_fk
+FOREIGN KEY (professor_id)
+REFERENCES flash.professor (professor_id)
+ON DELETE CASCADE
+ON UPDATE CASCADE
 NOT DEFERRABLE;
 
 ALTER TABLE flash.disciplina ADD CONSTRAINT disciplina_disciplina_fk
 FOREIGN KEY (requisito_equivalencia_disciplina_id)
 REFERENCES flash.disciplina (disciplina_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
+ON DELETE SET NULL
+ON UPDATE CASCADE
 NOT DEFERRABLE;
 
-ALTER TABLE flash.horario ADD CONSTRAINT disciplina_horario_fk
+ALTER TABLE flash.alocacao ADD CONSTRAINT disciplina_alocacao_fk
 FOREIGN KEY (disciplina_id)
 REFERENCES flash.disciplina (disciplina_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
+ON DELETE CASCADE
+ON UPDATE CASCADE
 NOT DEFERRABLE;
 
 
