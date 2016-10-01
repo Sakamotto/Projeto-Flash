6461af98237cf810a8bf7c1e1326f6d341bfd082
Danilo de Oliveira <danilo.psn94@gmail.com>
2016-10-01 12:13:38 -0300

    Alteração no modelo do banco e no banco

---
+++ 02.Banco de Dados/06.Testes Query SQL/Busca_Disciplias_Pre_Requisitos.sql
@@ -0,0 +1,14 @@
+﻿-- Essa consulta busca disciplinas que possuem uma determinada disciplina como pré-requisito.
+
+SELECT
+disciplina.nome AS DISCIPLINA
+
+FROM flash.disciplina
+INNER JOIN flash.requisito_disciplina
+ON requisito_disciplina.disciplina_id = disciplina.disciplina_id
+
+WHERE 
+
+requisito_disciplina.disciplina_pre_requisito_id IN (
+	SELECT disciplina.disciplina_id FROM flash.disciplina WHERE disciplina.nome ILIKE '%Sociologia%'
+)
