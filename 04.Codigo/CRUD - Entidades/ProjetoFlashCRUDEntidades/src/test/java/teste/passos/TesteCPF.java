5cce8c18b6e039aeb33a1949d482dbe67cad3069
Danilo de Oliveira <danilo.psn94@gmail.com>
2016-10-09 12:20:15 -0300

    BDD e TDD para CPF de professor adicionado

---
+++ 04.Codigo/CRUD - Entidades/ProjetoFlashCRUDEntidades/src/test/java/teste/passos/TesteCPF.java
@@ -0,0 +1,35 @@
+package teste.passos;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import model.dominio.Professor;
+
+import static org.junit.Assert.assertEquals;
+
+public class TesteCPF {
+    private String cpfInput;
+    private Professor professor;
+
+    @Given("^Eu entro com o CPF \"(.*)\"$")
+    public void testaCpf(String cpf) {
+        cpfInput = cpf;
+    }
+
+    @When("^Eu quero validar$")
+    public void validaCpf() {
+        professor = new Professor();
+        professor.setCpf(cpfInput, true);
+    }
+
+    @Then("^O cpf eh invalido$")
+    public void resultadoValidacaoInvalido() {
+        assertEquals(professor.getCpf(), null);
+    }
+
+    @Then("^O cpf eh valido$")
+    public void resultadoValidacaoValido() {
+        assertEquals(professor.getCpf(), cpfInput);
+    }
+
+}
