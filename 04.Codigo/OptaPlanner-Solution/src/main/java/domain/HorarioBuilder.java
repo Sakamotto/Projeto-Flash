01e896273499e34f26c9a50ea5fc70331de5d00b
Danilo de Oliveira <danilo.psn94@gmail.com>
2016-10-15 09:46:38 -0300

    Padrão de Projeto adicionado

---
+++ 04.Codigo/OptaPlanner-Solution/src/main/java/domain/HorarioBuilder.java
@@ -0,0 +1,34 @@
+package domain;
+
+/**
+ * Created by danilo on 15/10/16.
+ */
+public class HorarioBuilder {
+
+    Horario horario;
+
+    public HorarioBuilder(int id) {
+        horario = new Horario(id);
+    }
+
+    public HorarioBuilder addDiaSemana( Horario.DiaSemana diaSemana) {
+        horario.setDiaSemana(diaSemana);
+        return this;
+    }
+
+    public HorarioBuilder addHorarioInicio( int inicio, int fim ) {
+        horario.setHorarioInicio(inicio, fim);
+
+        return this;
+    }
+
+    public HorarioBuilder addHorarioFim( int inicio, int fim ) {
+        horario.setHorarioFim(inicio, fim);
+
+        return this;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+}
