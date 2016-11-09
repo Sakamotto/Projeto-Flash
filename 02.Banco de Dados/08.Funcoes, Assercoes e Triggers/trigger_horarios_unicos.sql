7a059de814f6af4fd93b29d072fcefdb7cce0344
Danilo de Oliveira <danilo.psn94@gmail.com>
2016-11-09 13:12:03 -0200

    Funcao e assercao

---
+++ 02.Banco de Dados/08.Funcoes, Assercoes e Triggers/trigger_horarios_unicos.sql
@@ -0,0 +1,21 @@
+﻿set schema 'flash';
+
+CREATE OR REPLACE FUNCTION horarios_unicos() RETURNS TRIGGER AS $$
+	BEGIN
+		IF EXISTS (
+			SELECT
+			    *
+			FROM horario h
+			WHERE h.hora_fim = NEW.hora_fim
+			    AND NEW.hora_inicio = h.hora_inicio
+		) THEN
+			RAISE EXCEPTION '%,% Esse registro já existe! ', NEW.hora_inicio, NEW.hora_fim;
+		END IF;
+
+		RETURN NEW;
+	END;
+$$ LANGUAGE PLPGSQL;
+
+CREATE TRIGGER horarios_unicos BEFORE INSERT OR UPDATE ON horario
+	FOR EACH ROW EXECUTE PROCEDURE horarios_unicos();
+
